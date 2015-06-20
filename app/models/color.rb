8e55a7f86a5d52e1d5e7c3e9f37356ae5f27e995
E Wing <emilypwing@gmail.com>
2015-06-20 09:05:30 -0700

    no message

--- app/models/color.rb
+++ app/models/color.rb
@@ -2,7 +2,7 @@ class Color < ActiveRecord::Base
 			belongs_to :car
 			has_many :finishes, dependent: :destroy
 
-				 accepts_nested_attributes_for :finishes, allow_destroy: true
+			 accepts_nested_attributes_for :finishes, allow_destroy: true
 
 
 end
